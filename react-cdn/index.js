c6a85e01272c21540b4f8f30a0c54b3867d33272
Debaditya Malakar <63513685+IronManCool001@users.noreply.github.com>
2020-11-29 13:19:08 +0530

    Add files via upload

---
+++ react-cdn/index.js
@@ -0,0 +1,13 @@
+function App(){
+    return (
+        <div>
+            <h2>Hello React CDN</h2>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <App />
+    </div>, document.getElementById('app')
+)
